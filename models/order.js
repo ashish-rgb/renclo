053ac18a63b7dcc045b472e2e70d88fcf4744dd9
prashant <nik.nikhilrider@gmail.com>
2020-04-27 21:29:00 +0530

    uploaded All

---
+++ models/order.js
@@ -0,0 +1,17 @@
+var mongoose=require("mongoose");
+ var orderSchema=new mongoose.Schema({
+  day:{type:Number,default:1},
+  size:{type:String,default:"s"},
+  productid:{
+    type:mongoose.Schema.Types.ObjectId,
+  ref:"product"},
+  address:String,
+  email:String,
+  phoneno:String,
+  firstname:String,
+  lastname:String
+})
+
+ var order=mongoose.model("order",orderSchema);
+
+ module.exports=order;
